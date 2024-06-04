cc2da85e288ca0166315bff4753f2d266c870e49
maxland255 <69438306+maxland255@users.noreply.github.com>
2024-06-04 22:06:53 +0200

    Initial commit

---
+++ library.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2024 pieteraerens.eu
+// All rights reserved.
+// The file library.cpp is a part of PythonDataType.
+// Last modified: 6/4/24 10:00 PM
+
+#include "library.h"
+
+#include <iostream>
+
+void hello() {
+    std::cout << "Hello, World!" << std::endl;
+}
---
+++ library.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2024 pieteraerens.eu
+// All rights reserved.
+// The file library.h is a part of PythonDataType.
+// Last modified: 6/4/24 10:05 PM
+
+#ifndef PYTHONDATATYPE_LIBRARY_H
+#define PYTHONDATATYPE_LIBRARY_H
+
+void hello();
+
+#endif //PYTHONDATATYPE_LIBRARY_H
